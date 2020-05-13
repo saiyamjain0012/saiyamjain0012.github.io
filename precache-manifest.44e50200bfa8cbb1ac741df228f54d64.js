87e3df58a62fac23222933711e04508e54a9a627
Saiyam Jain <saiyamjain0012@gmail.com>
2020-05-13 13:50:31 +0530

    fixed all files

---
+++ precache-manifest.44e50200bfa8cbb1ac741df228f54d64.js
@@ -0,0 +1,350 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "847478466601866560f9",
+    "url": "/css/chunk-058b2344.c73a2e1a.css"
+  },
+  {
+    "revision": "6a7d724f8159258c5dc1",
+    "url": "/css/chunk-15bf7b24.37a05521.css"
+  },
+  {
+    "revision": "cc8c627af7cb9b5e12a3",
+    "url": "/css/chunk-2c894af3.8df62af2.css"
+  },
+  {
+    "revision": "26639458ca903c640b7f",
+    "url": "/css/chunk-2d1a98c1.817741bf.css"
+  },
+  {
+    "revision": "862ee3f7489b01e15b07",
+    "url": "/css/chunk-32bb2de0.daa49e89.css"
+  },
+  {
+    "revision": "9b3c19010a55fac19013",
+    "url": "/css/chunk-4cf1c404.fd31cd03.css"
+  },
+  {
+    "revision": "dbf0967d7f79459087db",
+    "url": "/css/chunk-579437ca.403e36b5.css"
+  },
+  {
+    "revision": "e294a24881ec2b5e7d92",
+    "url": "/css/chunk-6ec8c437.c30d9e0d.css"
+  },
+  {
+    "revision": "c3b0733be6d3a8cd46bb",
+    "url": "/css/chunk-9e0d1682.c8329f4e.css"
+  },
+  {
+    "revision": "ada5bf6a1d2db7c853db",
+    "url": "/css/chunk-c6cd50fe.673f0042.css"
+  },
+  {
+    "revision": "647a4390225c56e400c3",
+    "url": "/css/chunk-fc5bef08.de9b4c7c.css"
+  },
+  {
+    "revision": "d0957a745aa053399ad6",
+    "url": "/css/chunk-vendors.649db783.css"
+  },
+  {
+    "revision": "69982db8cec221413749",
+    "url": "/css/index.9708e6e0.css"
+  },
+  {
+    "revision": "a8be8728ce45176a6987b075dcab02c4",
+    "url": "/fonts/brandico.a8be8728.eot"
+  },
+  {
+    "revision": "a9931e0ec5d5d7a77f1d3d8ae6b5d561",
+    "url": "/fonts/brandico.a9931e0e.woff"
+  },
+  {
+    "revision": "f85e2f68cbeac76ee61d04b019c78926",
+    "url": "/fonts/brandico.f85e2f68.ttf"
+  },
+  {
+    "revision": "0ea88478b62e92b5a6e6f6f40b1fa2b8",
+    "url": "/fonts/entypo.0ea88478.eot"
+  },
+  {
+    "revision": "5460a9ba11448e34f332f25e72dc16df",
+    "url": "/fonts/entypo.5460a9ba.woff"
+  },
+  {
+    "revision": "9ae5efdfcc29aa39a9c1e1444d1ebdf8",
+    "url": "/fonts/entypo.9ae5efdf.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "348f9d2067cb5824b8b6baa7fce9b413",
+    "url": "/fonts/fontelico.348f9d20.woff"
+  },
+  {
+    "revision": "395c2954b63f688ee82cedc8ab834c57",
+    "url": "/fonts/fontelico.395c2954.eot"
+  },
+  {
+    "revision": "df854b0cef01fcf8f74e6c3a3017ff32",
+    "url": "/fonts/fontelico.df854b0c.ttf"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/fonts/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/fonts/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/fonts/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/fonts/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "6e4b2cea6a930984c21faf7c8d4cc008",
+    "url": "/fonts/iconicstroke.6e4b2cea.ttf"
+  },
+  {
+    "revision": "a83f7dbab4425cea9f0ad48b46569124",
+    "url": "/fonts/iconicstroke.a83f7dba.eot"
+  },
+  {
+    "revision": "c0b29cc48c75a2010fca80b6f9c64985",
+    "url": "/fonts/iconicstroke.c0b29cc4.woff"
+  },
+  {
+    "revision": "96f1c901c087fb64019f7665f7f8aca6",
+    "url": "/fonts/ionicons.96f1c901.woff2"
+  },
+  {
+    "revision": "99b863497156d4478ec3489fefb3815c",
+    "url": "/fonts/ionicons.99b86349.woff"
+  },
+  {
+    "revision": "a558ac78b554eefa181737749366a91c",
+    "url": "/fonts/ionicons.a558ac78.eot"
+  },
+  {
+    "revision": "ef4a9f280b0e411ddf6c930a0a37c2b0",
+    "url": "/fonts/ionicons.ef4a9f28.ttf"
+  },
+  {
+    "revision": "13c94e616365b00263183ea2761a06c7",
+    "url": "/fonts/maki.13c94e61.eot"
+  },
+  {
+    "revision": "366c17d53bc17d0e68c9a81e4f0a7755",
+    "url": "/fonts/maki.366c17d5.woff"
+  },
+  {
+    "revision": "5a5747f9c12ec4b563a7205f5db4cc4b",
+    "url": "/fonts/maki.5a5747f9.ttf"
+  },
+  {
+    "revision": "06201b3fa7ef64980627c2f3515a3761",
+    "url": "/fonts/openwebicons.06201b3f.eot"
+  },
+  {
+    "revision": "2ecaa60ecb8ad7b6d662a9abab39ba57",
+    "url": "/fonts/openwebicons.2ecaa60e.ttf"
+  },
+  {
+    "revision": "9dd24e3a41e9e40b51349d12e870ebbc",
+    "url": "/fonts/openwebicons.9dd24e3a.woff"
+  },
+  {
+    "revision": "90f6882a8306b10faaab780e5c5d2cc8",
+    "url": "/fonts/vuestic-icons.90f6882a.eot"
+  },
+  {
+    "revision": "cc2f8e936020ce7168762413c50082bb",
+    "url": "/fonts/vuestic-icons.cc2f8e93.ttf"
+  },
+  {
+    "revision": "279ec925f7246d06e99a508971df9830",
+    "url": "/fonts/zocial-regular-webfont.279ec925.eot"
+  },
+  {
+    "revision": "2b2d7cdb3e09d03b7a97152298a0bfea",
+    "url": "/fonts/zocial-regular-webfont.2b2d7cdb.ttf"
+  },
+  {
+    "revision": "53bbf5192e5a972cb76108dae88d63e9",
+    "url": "/fonts/zocial-regular-webfont.53bbf519.woff"
+  },
+  {
+    "revision": "7e7084e5f5f7990e2dcd9d8bdc150769",
+    "url": "/img/brandico.7e7084e5.svg"
+  },
+  {
+    "revision": "d82bd106ff8e8e19e6bbde532ad9ff0b",
+    "url": "/img/entypo.d82bd106.svg"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "265aac87e2c2bd6d5572ec0985851cf3",
+    "url": "/img/fontelico.265aac87.svg"
+  },
+  {
+    "revision": "15d5b197da33ca307d805e7dce5bca5c",
+    "url": "/img/glyphicons-halflings-regular.15d5b197.svg"
+  },
+  {
+    "revision": "0517f35a8b7ab59b00c3c509ce9abd5f",
+    "url": "/img/iconicstroke.0517f35a.svg"
+  },
+  {
+    "revision": "d659209138fc7c28c23a48496bdd1c8b",
+    "url": "/img/ionicons.d6592091.svg"
+  },
+  {
+    "revision": "e991c2ed305cbe05d3a9a8c1d98b8c94",
+    "url": "/img/maki.e991c2ed.svg"
+  },
+  {
+    "revision": "61a0dd2d13bf68c664dfcb1034d798f3",
+    "url": "/img/menu-collapsed.61a0dd2d.svg"
+  },
+  {
+    "revision": "60e0e4cd7102b715d6fcd87e78d1720e",
+    "url": "/img/menu-expanded.60e0e4cd.svg"
+  },
+  {
+    "revision": "5319290f7d58090f09b3ad141259ac8e",
+    "url": "/img/nav-color.5319290f.svg"
+  },
+  {
+    "revision": "ae4b6b974e99a26ae1882d606b4f438f",
+    "url": "/img/nav-messages.ae4b6b97.svg"
+  },
+  {
+    "revision": "8a9e251269c2df20664dd1039babea5f",
+    "url": "/img/nav-notifications.8a9e2512.svg"
+  },
+  {
+    "revision": "f0249cd6c3a360cb7406ab141da4e859",
+    "url": "/img/nav-search.f0249cd6.svg"
+  },
+  {
+    "revision": "3cc62e840f787a265e10c429d1701976",
+    "url": "/img/openwebicons.3cc62e84.svg"
+  },
+  {
+    "revision": "1d6148cdb6ae3c80bfd16725dc949562",
+    "url": "/img/vuestic-icons.1d6148cd.svg"
+  },
+  {
+    "revision": "a4ea8a81181d2f2b2009bbe5408d3740",
+    "url": "/img/zocial-regular-webfont.a4ea8a81.svg"
+  },
+  {
+    "revision": "101dc6b79143f5aa5bc79975b571afa7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "847478466601866560f9",
+    "url": "/js/chunk-058b2344.66881ff5.js"
+  },
+  {
+    "revision": "6a7d724f8159258c5dc1",
+    "url": "/js/chunk-15bf7b24.95321a86.js"
+  },
+  {
+    "revision": "6336ace66e22da212021",
+    "url": "/js/chunk-216b3f17.5e7e8451.js"
+  },
+  {
+    "revision": "cc8c627af7cb9b5e12a3",
+    "url": "/js/chunk-2c894af3.1fe97f22.js"
+  },
+  {
+    "revision": "5434f326599647fcf74d",
+    "url": "/js/chunk-2d0b1639.fe356606.js"
+  },
+  {
+    "revision": "e2fdd6bd48cf17029004",
+    "url": "/js/chunk-2d0d3ed3.ec29d59f.js"
+  },
+  {
+    "revision": "26639458ca903c640b7f",
+    "url": "/js/chunk-2d1a98c1.50091099.js"
+  },
+  {
+    "revision": "9cfdece780720000628e",
+    "url": "/js/chunk-2d2089ce.005db87c.js"
+  },
+  {
+    "revision": "b2bf57f4bbe8da66270b",
+    "url": "/js/chunk-2d22c340.6f576084.js"
+  },
+  {
+    "revision": "076efa8b90fa005a30cd",
+    "url": "/js/chunk-2d22c838.0f0907ad.js"
+  },
+  {
+    "revision": "862ee3f7489b01e15b07",
+    "url": "/js/chunk-32bb2de0.a33e3486.js"
+  },
+  {
+    "revision": "9b3c19010a55fac19013",
+    "url": "/js/chunk-4cf1c404.9c8c0df9.js"
+  },
+  {
+    "revision": "dbf0967d7f79459087db",
+    "url": "/js/chunk-579437ca.9bf687df.js"
+  },
+  {
+    "revision": "e294a24881ec2b5e7d92",
+    "url": "/js/chunk-6ec8c437.376f39a1.js"
+  },
+  {
+    "revision": "c3b0733be6d3a8cd46bb",
+    "url": "/js/chunk-9e0d1682.c56aee39.js"
+  },
+  {
+    "revision": "ada5bf6a1d2db7c853db",
+    "url": "/js/chunk-c6cd50fe.573e963b.js"
+  },
+  {
+    "revision": "647a4390225c56e400c3",
+    "url": "/js/chunk-fc5bef08.48c20784.js"
+  },
+  {
+    "revision": "d0957a745aa053399ad6",
+    "url": "/js/chunk-vendors.0855cdbc.js"
+  },
+  {
+    "revision": "69982db8cec221413749",
+    "url": "/js/index.fd639f61.js"
+  },
+  {
+    "revision": "11a4ea455710ea83a52313d848324da8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
